5fb34c735f4ccb5fa063d7af7deca947289e8c4c
RafliSubhi <raflisubhi11@gmail.com>
2024-09-22 22:41:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+
+const serviceID = "service_db0n20d";
+const templateID = "template_82si62y"
+
+emailjs.send(serviceID, templateID, params)
+    .then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("your message sent successfully");
+        }
+    )
+    .catch((err) => console.log(err));
+}
